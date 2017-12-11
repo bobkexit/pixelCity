dbe3ccb893d2b80722ec9565106ba19418c0c188
Bob Kexit <bobkexit@gmail.com>
2017-12-11 13:05:39 +0300

    init project

--- pixel-city/Controllers/MapVC.swift
+++ pixel-city/Controllers/MapVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MapVC.swift
 //  pixel-city
 //
 //  Created by Николай Маторин on 11.12.2017.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MapVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()

f9712ce1027a155d9aeb630e1a7c9ff19dd1c89b
Bob Kexit <bobkexit@gmail.com>
2017-12-12 12:23:14 +0300

    added Assets, animated Photos View

--- pixel-city/Controllers/MapVC.swift
+++ pixel-city/Controllers/MapVC.swift
@@ -7,19 +7,95 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class MapVC: UIViewController {
-
+class MapVC: UIViewController, UIGestureRecognizerDelegate {
+    
+    // Outlets
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var pullUpViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var pullUpView: UIView!
+    
+    //Variables
+    var locationManager = CLLocationManager()
+    
+    //Constants
+    var authStatus = CLLocationManager.authorizationStatus()
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        mapView.delegate = self
+        locationManager.delegate = self
+        
+        configureLocationServices()
+        addDoubleTap()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func centerMapBtnPressed(_ sender: Any) {
+        if authStatus == .authorizedAlways || authStatus == .authorizedWhenInUse {
+            centerMapOnUserLocation()
+        }
     }
-
-
+    
+    func addDoubleTap() {
+        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(MapVC.dropPin(sender:)))
+        doubleTap.numberOfTapsRequired = 2
+        doubleTap.delegate = self
+        mapView.addGestureRecognizer(doubleTap)
+    }
+    
+    func animateViewUp() {
+        pullUpViewHeightConstraint.constant = 300
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+    }
 }
 
+extension MapVC: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        }
+        
+        let pinAnnotation = MKPinAnnotationView(annotation: annotation, reuseIdentifier: DROPPABLE_PIN)
+        pinAnnotation.pinTintColor = #colorLiteral(red: 0.9771530032, green: 0.7062081099, blue: 0.1748393774, alpha: 1)
+        pinAnnotation.animatesDrop = true
+        return pinAnnotation
+    }
+    
+    func centerMapOnUserLocation() {
+        guard let coordinate = locationManager.location?.coordinate else { return }
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(coordinate, REGION_RADIUS * 2.0, REGION_RADIUS * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    @objc func dropPin(sender: UITapGestureRecognizer) {
+        mapView.removeAnnotations(mapView.annotations)
+        animateViewUp()
+        let touchPoint = sender.location(in: mapView)
+        let touchCoordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+        
+        let annotation = DroppablePin(coordinate: touchCoordinate, identifier: DROPPABLE_PIN)
+        mapView.addAnnotation(annotation)
+        
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(touchCoordinate, REGION_RADIUS * 2.0, REGION_RADIUS * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+}
+
+extension MapVC: CLLocationManagerDelegate {
+    func configureLocationServices() {
+        if authStatus == .notDetermined {
+            locationManager.requestAlwaysAuthorization()
+        } else {
+            return
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        authStatus = status
+        centerMapOnUserLocation()
+    }
+}
---
+++ pixel-city/Utilities/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pixel-city
+//
+//  Created by Николай Маторин on 12.12.2017.
+//  Copyright © 2017 Николай Маторин. All rights reserved.
+//
+
+import Foundation
+
+let DROPPABLE_PIN = "droppablePin"
+
+let REGION_RADIUS: Double = 1000
+
